51e46d98df05676b0fc7530f52df6e753317c30d
Llonvne <work@llonvne.cn>
2022-01-10 10:00:04 +0800

    Create Hailstone.cpp

---
+++ src/Hailstone/Hailstone.cpp
@@ -0,0 +1,20 @@
+class Hailstone{
+public:
+    static vector<int> HailstoneGenerator(int start){
+        vector<int> result;
+        result.push_back(start);
+        while (start != 1){
+            if (start % 2 == 1){
+                start = start * 3 + 1;
+            }
+            else {
+                start /= 2;
+            }
+            result.push_back(start);
+        }
+        return result;
+    }
+    static int getHailstoneLength(int start){
+        return (int)HailstoneGenerator(start).size();
+    }
+};
