38fa62f773d8fbec16e629c1844d0ac91290b21e
Llonvne <work@llonvne.cn>
2022-01-12 09:42:33 +0800

    Update fibnacci_learn.cpp

--- src/fibnacci/fibnacci_learn.cpp
+++ src/fibnacci/fibnacci_learn.cpp
@@ -2,3 +2,13 @@
 int fib(int n){
     return n < 2 ? n : fib(n-1) + fib(n-2);
 }
+
+// Fibnacci 迭代动态规划实现
+int fib(int n) {
+    int f = 0;
+    int g = 1;
+    while (n--){
+        int t = f;f = g;g = t;
+    }
+    return g;
+}
