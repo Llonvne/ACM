94a838ca0c33a60c3ac351f0264d730e861c0407
Llonvne <work@llonvne.cn>
2022-01-17 15:39:37 +0800

    Update HanoiTower.cpp

--- src/HanoiTower汉诺塔问题/HanoiTower.cpp
+++ src/HanoiTower汉诺塔问题/HanoiTower.cpp
@@ -1,6 +1,7 @@
 #include <stack>
 using namespace std;
 
+// 汉诺塔问题解决 O(2^N)
 #define SOURCE // 标识汉诺塔源
 #define TARGET // 标识汉诺塔目标
 #define BUFFER // 汉诺塔缓存
@@ -15,3 +16,10 @@ void Hanoi(stack<int>& SOURCE A,stack<int>& BUFFER B,stack<int>& TARGET C,int pr
         Hanoi(SOURCE B,BUFFER A,TARGET C,problemSize-1);
     }
 }
+
+/*
+ * 不难发现当只有一个盘子的时候复杂度为 O(1)
+ * 每次递归都会产生两个新问题但是问题规模都减少了 1
+ * 递推式子为 T(n) = 2 * T(n-1) + O(1)
+ * 不难算出 T(n) = O(2^n)
+*/
