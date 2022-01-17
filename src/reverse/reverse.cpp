580e4f13f6f3fdef427c3fb2df3dd18a3f1525f8
Llonvne <work@llonvne.cn>
2022-01-17 10:33:19 +0800

    调整了Reverse算法的说明

--- src/reverse/reverse.cpp
+++ src/reverse/reverse.cpp
@@ -7,7 +7,7 @@ void reverse(int A[],int lo,int hi){
         swap(A[lo++],A[--hi]);
     }
 }
-// 减而治之 递归 [lo,hi)
+// 数组倒置 递归版本 [lo,hi) 减而治之 
 void reverseRecursion(int A[],int lo,int hi){
     if (lo < hi){
         swap(A[lo],A[hi-1]);
