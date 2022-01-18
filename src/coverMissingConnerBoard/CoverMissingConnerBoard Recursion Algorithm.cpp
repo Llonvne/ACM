1402f1b2aa131cc9bf105f03e28edb5f46d6aa69
Llonvne <work@llonvne.cn>
2022-01-18 12:09:58 +0800

    Update CoverMissingConnerBoard Recursion Algorithm.cpp

--- src/coverMissingConnerBoard/CoverMissingConnerBoard Recursion Algorithm.cpp
+++ src/coverMissingConnerBoard/CoverMissingConnerBoard Recursion Algorithm.cpp
@@ -5,14 +5,28 @@ class CoverMissingConnerBoard{
     // 辨识每块木块的颜色标记
     uint64_t _color;
     
-    // 覆盖递归算法
+    // cover 算法
     void cover(int n,int x,int y,int dx,int dy){
+        
+        // 子问题的阶次
         int s = 1 << (n-1);
+        
+        // 在中心放置一块缺口朝棋盘缺口的L型
         place(x + dx*(s-1),y + dy*(s-1),dx,dy);
+        
+        // 在问题 > 1 时 
         if (1 < n){
+            
+            // 正向缺口方向覆盖
             cover(n-1,x,y,dx,dy);
+            
+            // 逆向缺口方向覆盖
             cover(n-1,x + dx * s,y + dy * s,dx,dy);
+            
+            // 垂直缺口一个方向
             cover(n-1,x + dx*(2*s - 1),y,-dx,dy);
+            
+            // 垂直缺口另外一个方向
             cover(n-1,x,y + dy*(2*s -1),dx,-dy);
         }
     }
