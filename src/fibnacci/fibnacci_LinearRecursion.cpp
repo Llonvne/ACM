7ab80c8bb49d6c9d93267ac804df70a53bc17720
Llonvne <work@llonvne.cn>
2022-01-18 09:41:31 +0800

    修正了错误的Fibnacci数列线性递归算法

--- src/fibnacci/fibnacci_LinearRecursion.cpp
+++ src/fibnacci/fibnacci_LinearRecursion.cpp
@@ -1,11 +1,12 @@
 // fibnacci 数列线性递归 O(n)
 // prev 用来记录前一项
-uint64_t fib(int n,uint64_t& prev){
+int64_t fib(int n,int64_t& prev){
     if (0 == n){
         // fib(-1) = 1,fib(0) = 0; 直接取 0
         prev = 1;
         return 0;
     }
-    uint64_t prevPrev = fib(n-1,prev); // 算出前两项
-    return prev + prevPrev; 
+    int64_t prevPrev;
+    prev = fib(n-1,prevPrev);
+    return prev + prevPrev;
 }
