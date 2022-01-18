bf9cf1ec16edd0347ab081dcecdb459af1b7bec2
Llonvne <work@llonvne.cn>
2022-01-18 15:01:19 +0800

    增加了Ackermann函数的描述

--- src/ackermann/ackermann.cpp
+++ src/ackermann/ackermann.cpp
@@ -1,4 +1,8 @@
 // Ackermann Recursion Algorithm 
+// Baidu Baike : https://baike.baidu.com/item/阿克曼函数/10988285?fr=aladdin
+// Ackermann函数定义如下：　若m=0，返回n+1。
+// 若m>0且n=0，返回Ackermann(m-1,1)。
+// 若m>0且n>0，返回Ackermann(m-1,Ackermann(m,n-1))。
 int ackermann(int m,int n){
     if (m == 0){
         return n + 1;
