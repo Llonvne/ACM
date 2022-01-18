59f2290797dc83d96d9adf3ddd7c38bf63386ae3
Llonvne <work@llonvne.cn>
2022-01-18 22:45:30 +0800

    分离引用计数但是不抽象

---
+++ src/Handle/Handle分离引用计数.cpp
@@ -0,0 +1,48 @@
+// Point 类
+class Point{
+private:
+    int xval;
+    int yval;
+public:
+    Point() : xval(0),yval(0){};
+    Point(const int x,const int y): xval(x),yval(y){};
+    
+    int x() const {
+        return xval;
+    }
+    int y() const {
+        return yval;
+    }
+    
+    Point x(const int x){
+        xval = x;
+        return *this;
+    }
+    Point y(const int y){
+        yval = y;
+        return *this;
+    }
+};
+class Handle {    
+public:
+    // 构造函数
+    Handle();
+    Handle(const int x,const int y);
+    Handle(const Point& p);
+    Handle(const Handle& h);
+    // 析构函数
+    ~Handle() {};
+    // 赋值
+    Handle& operator= (const Handle& h){}
+
+    // 成员操作函数
+    int x() const;
+    Handle& x(const int x); 
+    int y() const ;
+    Handle& y(const int y);
+private:
+    // 点
+    Point * p;
+    // 计数
+    int * u;
+};
